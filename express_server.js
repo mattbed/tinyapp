81209884dcb70568fc59833ad580d7126e2191cc
Matthew <m.bednarski@gmail.com>
2022-01-25 02:19:43 +0000

    added form functionality

--- express_server.js
+++ express_server.js
@@ -24,6 +24,11 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
